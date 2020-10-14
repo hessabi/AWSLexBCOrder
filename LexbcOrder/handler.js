8dec5491d63719ad99398cc99a39cb7de5d8be78
hessabi <amir.hessabi@bigcommerce.com>
2020-10-14 13:13:32 -0500

    first commit

---
+++ LexbcOrder/handler.js
@@ -0,0 +1,98 @@
+"use strict";
+const request = require("request-promise");
+const BigCommerce = require("node-bigcommerce");
+
+const bigCommerce = new BigCommerce({
+  logLevel: "info",
+  clientId: process.env.BC_CLIENT,
+  accessToken: process.env.BC_TOKEN,
+  storeHash: process.env.STORE_HASH,
+  responseType: "json",
+  apiVersion: "v2"
+});
+
+async function createBCOrder() {
+  const options = {
+    method: "PUT",
+    uri: `https://api.bigcommerce.com/stores/${process.env.STORE_HASH}/v2/orders/`,
+    headers: {
+      accept: "application/json",
+      "X-Auth-Client": process.env.BC_CLIENT,
+      "X-Auth-Token": process.env.BC_TOKEN
+    },
+    body: {
+      {
+        "status_id": 0,
+        "customer_id": 11,
+        "billing_address": {
+          "first_name": "Jane",
+          "last_name": "Doe",
+          "street_1": "123 Main Street",
+          "city": "Austin",
+          "state": "Texas",
+          "zip": "78751",
+          "country": "United States",
+          "country_iso2": "US",
+          "email": "janedoe@email.com"
+        },
+        "shipping_addresses": [
+          {
+            "first_name": "Trish",
+            "last_name": "Test",
+            "company": "Acme Pty Ltd",
+            "street_1": "666 Sussex St",
+            "city": "Austin",
+            "state": "Texas",
+            "zip": "78751",
+            "country": "United States",
+            "country_iso2": "US",
+            "email": "elsie@example.com"
+          }
+        ],
+        "products": [
+          {
+            "name": "BigCommerce Motor Oil",
+            "quantity": 1,
+            "price_inc_tax": 10.98,
+            "price_ex_tax": 10
+          },
+          {
+            "name": "BigCommerce Oil Filter",
+            "quantity": 1,
+            "price_inc_tax": 50,
+            "price_ex_tax": 45
+          }
+        ]
+      }
+    }
+  };
+  var transactionData = await request(options);
+  console.log("transaction", transactionData);
+  return transactionData;
+}
+
+
+module.exports.lexbcOrder = async function (event, context, callback) => {
+
+  callback(null, {
+    "dialogAction": {
+      "type": "Close",
+      "fulfillmentState": "Fulfilled",
+      "message": {
+        "contentType": "PlainText",
+        "content": "Lex Testing"
+      }
+    }
+  }
+
+);
+
+if(something){
+  const createOrder = await createBCOrder();
+  console.log("createOrder", createOrder);
+}else{
+
+  console.log("failed", failed);
+}
+
+};
---
+++ README.md
@@ -0,0 +1,63 @@
+## What
+
+Boilerplate serverless function to enable Stripe Subscriptions.
+
+## What does this application do?
+
+The application is using BigCommerce webhooks to send Order data to a serverless function to determine if the user purchased a subscription product and should be enrolled in a Stripe subscription.
+
+## Contributing
+
+George FitzGibbons
+
+### Running the project
+
+To get started you will need to have a BigCommerce Store.
+
+You will need to have +v10 node.
+
+You will need Serverless
+
+```https://serverless.com/
+
+In this example the serverless.yml is configured for AWS.
+https://serverless.com/framework/docs/providers/aws/guide/installation/
+
+You can easily update the yml for your desired FAAS providers
+```
+
+You will need to generate BigCommerce API keys, these keys need to have read permissions for products.
+
+In the serverless.yml file update the environment with your site API Keys
+
+```
+environment:
+  STORE_HASH: {YOUR STORE HASH}
+  BC_CLIENT: {BC CLIENT ID}
+  BC_TOKEN: {BC TOKEN ID}
+  STRIPE_SECRET: {STRIPE SECRET KEY}
+
+```
+
+Now run to set up
+
+```bash
+npm install
+```
+
+Now you're ready to deploy
+
+```bash
+cd lexbcOrder
+sls deploy
+```
+
+
+
+
+You will get an API endpoint back, you will use this when you set up your webhook.
+
+```
+endpoints:
+  POST - https://{XXXXXX}.execute-api.us-east-1.amazonaws.com/dev/stripeManager
+```
