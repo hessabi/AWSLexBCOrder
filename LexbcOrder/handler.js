eb35d02b8d5b9c45392b4ea1199878646779ebb1
hessabi <amir.hessabi@bigcommerce.com>
2020-10-14 18:39:33 -0500

    updates

--- LexbcOrder/handler.js
+++ LexbcOrder/handler.js
@@ -11,7 +11,7 @@ const bigCommerce = new BigCommerce({
   apiVersion: "v2"
 });
 
-async function createBCOrder() {
+async function createBCOrder(product) {
   const options = {
     method: "PUT",
     uri: `https://api.bigcommerce.com/stores/${process.env.STORE_HASH}/v2/orders/`,
@@ -37,30 +37,24 @@ async function createBCOrder() {
         },
         "shipping_addresses": [
           {
-            "first_name": "Trish",
-            "last_name": "Test",
+            "first_name": "Jane",
+            "last_name": "Doe",
             "company": "Acme Pty Ltd",
-            "street_1": "666 Sussex St",
+            "street_1": "123 Main Street",
             "city": "Austin",
             "state": "Texas",
             "zip": "78751",
             "country": "United States",
             "country_iso2": "US",
-            "email": "elsie@example.com"
+            "email": "janedoe@email.com"
           }
         ],
         "products": [
           {
-            "name": "BigCommerce Motor Oil",
+            "name": product,
             "quantity": 1,
             "price_inc_tax": 10.98,
             "price_ex_tax": 10
-          },
-          {
-            "name": "BigCommerce Oil Filter",
-            "quantity": 1,
-            "price_inc_tax": 50,
-            "price_ex_tax": 45
           }
         ]
       }
@@ -73,6 +67,19 @@ async function createBCOrder() {
 
 
 module.exports.lexbcOrder = async function (event, context, callback) => {
+
+  var serviceType = event.currentIntent.slots.serviceType,
+      service = "We don't offer that service";
+
+console.log("serviceType", serviceType);
+
+      if(serviceType != null){
+        const createOrder = await createBCOrder(serviceType);
+        console.log("createOrder", createOrder);
+      }else{
+
+        console.log("failed", failed);
+      }
 
   callback(null, {
     "dialogAction": {
@@ -87,12 +94,6 @@ module.exports.lexbcOrder = async function (event, context, callback) => {
 
 );
 
-if(something){
-  const createOrder = await createBCOrder();
-  console.log("createOrder", createOrder);
-}else{
 
-  console.log("failed", failed);
-}
 
 };
