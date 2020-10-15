33cd8ebeb0ccbf5be41d618fa2f007c080a035a5
hessabi <amir.hessabi@bigcommerce.com>
2020-10-14 22:29:38 -0500

    updates

--- LexbcOrder/handler.js
+++ LexbcOrder/handler.js
@@ -1,36 +1,25 @@
 "use strict";
-const request = require("request-promise");
+const BigCommerce = require("node-bigcommerce");
 
+const bigCommerce = new BigCommerce({
+  logLevel: "info",
+  clientId: process.env.BC_CLIENT,
+  accessToken: process.env.BC_TOKEN,
+  storeHash: process.env.STORE_HASH,
+  responseType: "json",
+  apiVersion: "v2"
+});
 
 async function createBCOrder(product) {
-  const options = {
-    method: "PUT",
-    uri: `https://api.bigcommerce.com/stores/${process.env.STORE_HASH}/v2/orders/`,
-    headers: {
-      accept: "application/json",
-      "X-Auth-Client": process.env.BC_CLIENT,
-      "X-Auth-Token": process.env.BC_TOKEN
-    },
-    body: JSON.stringify({
+  try{
+    const resData = await bigCommerce.post('/orders', {
+      body: JSON.stringify({
 
-      "status_id": 0,
-      "customer_id": 11,
-      "billing_address": {
-        "first_name": "Jane",
-        "last_name": "Doe",
-        "street_1": "123 Main Street",
-        "city": "Austin",
-        "state": "Texas",
-        "zip": "78751",
-        "country": "United States",
-        "country_iso2": "US",
-        "email": "janedoe@email.com"
-      },
-      "shipping_addresses": [
-        {
+        "status_id": 0,
+        "customer_id": 11,
+        "billing_address": {
           "first_name": "Jane",
           "last_name": "Doe",
-          "company": "Acme Pty Ltd",
           "street_1": "123 Main Street",
           "city": "Austin",
           "state": "Texas",
@@ -38,22 +27,37 @@ async function createBCOrder(product) {
           "country": "United States",
           "country_iso2": "US",
           "email": "janedoe@email.com"
-        }
-      ],
-      "products": [
-        {
-          "name": product,
-          "quantity": 1,
-          "price_inc_tax": 10.98,
-          "price_ex_tax": 10
-        }
-      ]
+        },
+        "shipping_addresses": [
+          {
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "company": "Acme Pty Ltd",
+            "street_1": "123 Main Street",
+            "city": "Austin",
+            "state": "Texas",
+            "zip": "78751",
+            "country": "United States",
+            "country_iso2": "US",
+            "email": "janedoe@email.com"
+          }
+        ],
+        "products": [
+          {
+            "name": product,
+            "quantity": 1,
+            "price_inc_tax": 10.98,
+            "price_ex_tax": 10
+          }
+        ]
 
+      })
     })
-  };
-  var transactionData = await request(options);
-  console.log("transaction", transactionData);
-  return transactionData;
+    console.log("resData",resData);
+    return resData;
+  } catch(err){
+    console.error(err);
+  }
 }
 
 
@@ -64,7 +68,8 @@ module.exports.lexbcOrder = async (event, context, callback) => {
 
     console.log("productType", productType);
 
-    if(productType == "Motor Oil"){
+    if(productType !== ""){
+      console.log("productType", productType);
       const createOrder = await createBCOrder(productType);
       console.log("createOrder", createOrder);
 
