b077e1bbf40583df4aed10a526904e3ed38854ef
hessabi <amir.hessabi@bigcommerce.com>
2020-10-16 08:53:03 -0500

    <message>

---
+++ Theme/assets/js/theme/global/quick-view.js
@@ -0,0 +1,30 @@
+import 'foundation-sites/js/foundation/foundation';
+import 'foundation-sites/js/foundation/foundation.dropdown';
+import utils from '@bigcommerce/stencil-utils';
+import ProductDetails from '../common/product-details';
+import { defaultModal, modalTypes } from './modal';
+import 'slick-carousel';
+
+export default function (context) {
+    const modal = defaultModal();
+
+    $('body').on('click', '.quickview', event => {
+        event.preventDefault();
+
+        const productId = $(event.currentTarget).data('productId');
+
+        modal.open({ size: 'large' });
+
+        utils.api.product.getById(productId, { template: 'products/quick-view' }, (err, response) => {
+            modal.updateContent(response);
+
+            modal.$content.find('.productView').addClass('productView--quickView');
+
+            modal.$content.find('[data-slick]').slick();
+
+            modal.setupFocusableElements(modalTypes.QUICK_VIEW);
+
+            return new ProductDetails(modal.$content.find('.quickView'), context);
+        });
+    });
+}
