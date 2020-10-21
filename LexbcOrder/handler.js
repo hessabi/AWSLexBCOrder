c5e8441154d4d1ae35bd09297e6c92cdd0180425
hessabi <amir.hessabi@bigcommerce.com>
2020-10-21 14:24:38 -0500

    connection to products>

--- LexbcOrder/handler.js
+++ LexbcOrder/handler.js
@@ -1,9 +1,9 @@
 "use strict";
 const request = require("request-promise");
 
-async function createBCOrder() {
+async function createBCOrder(product_id) {
 
-  try{
+  try {
 
     const orderCreate = {
       method: "POST",
@@ -16,20 +16,24 @@ async function createBCOrder() {
       },
       body:
 
-          { status_id: 8,
-            customer_id: 1,
-            billing_address:
-            { first_name: 'Amir',
-            last_name: 'Hessabi',
-            street_1: '123 Main Street',
-            city: 'Austin',
-            state: 'Texas',
-            zip: '78751',
-            country: 'United States',
-            country_iso2: 'US',
-            email: 'amir.hessabi@bigcommerce.com' },
-            shipping_addresses:
-            [ { first_name: 'Amir',
+      {
+        status_id: 8,
+        customer_id: 1,
+        billing_address:
+        {
+          first_name: 'Amir',
+          last_name: 'Hessabi',
+          street_1: '123 Main Street',
+          city: 'Austin',
+          state: 'Texas',
+          zip: '78751',
+          country: 'United States',
+          country_iso2: 'US',
+          email: 'amir.hessabi@bigcommerce.com'
+        },
+        shipping_addresses:
+          [{
+            first_name: 'Amir',
             last_name: 'Hessabi',
             company: 'BigCommerce',
             street_1: '123 Main Street',
@@ -38,37 +42,81 @@ async function createBCOrder() {
             zip: '78751',
             country: 'United States',
             country_iso2: 'US',
-            email: 'amir.hessabi@bigcommerce.com' } ],
-            products: [ {
-              product_id: 112,
-              quantity: 2
-            } ]
-          },
+            email: 'amir.hessabi@bigcommerce.com'
+          }],
+        products: [{
+          product_id: product_id,
+          quantity: 2
+        }]
+      },
       json: true
     };
-    console.log("orderCreate",orderCreate);
+    console.log("orderCreate", orderCreate);
     var resData = await request(orderCreate);
 
-    console.log("resData",resData);
+    console.log("resData", resData);
     return resData;
 
-  } catch(err){
+  } catch (err) {
     console.error(err);
   }
 
 }
 
+function getProductID(type) {
+
+  switch (type) {
+    case "oil change":
+      return 112;
+
+    case "wiper blades":
+      return 119;
+
+    case "brake fluid service":
+      return 118;
+
+    case "battery service":
+      return 117;
+
+    case "radiator service":
+      return 116;
+
+    case "cooling service":
+      return 120;
+
+    case "filters":
+      return 114;
+
+    case "differential fluid service":
+      return 113;
+
+    case "transmission system service":
+      return 115;
+
+    default:
+      return 0;
+
+  }
+
+}
 
 module.exports.lexbcOrder = async (event, context, callback) => {
 
-  try{
-    var productType = JSON.stringify(event.currentIntent.slots.ProductType);
+  try {
+    var productType = event.currentIntent.slots.ProductType;
+
+    const prodID = getProductID(productType)
 
     console.log("productType", productType);
 
-    if(productType !== ""){
+    console.log("productID", prodID);
+    console.log("productID type", typeof prodID);
+
+    if (productType !== "" && prodID) {
       console.log("productType", productType);
-      const createOrder = await createBCOrder();
+      console.log("productID", prodID);
+      console.log("productID type", typeof prodID);
+      const createOrder = await createBCOrder(prodID);
       console.log("createOrder", createOrder);
 
 
@@ -82,8 +130,19 @@ module.exports.lexbcOrder = async (event, context, callback) => {
           }
         }
       })
-    }else {
-      console.log("If statement failed");
+    } else {
+      console.log("We don't have a matching product", productType);
+
+      callback(null, {
+        "dialogAction": {
+          "type": "Close",
+          "fulfillmentState": "Failed",
+          "message": {
+            "contentType": "PlainText",
+            "content": "We don't offer that service"
+          }
+        }
+      })
     }
 
 
